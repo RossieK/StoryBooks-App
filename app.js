b66ed6085eb81ef809bfce2e1c7357c527240f3f
RossieK <rosi.kirova96@gmail.com>
2021-01-23 22:04:04 +0200

    Add MongoDB connection

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const connectDB = require('./config/db');
 
 // Load config
 dotenv.config({ path: './config/config.env' });
+
+// Connect DB
+connectDB();
 
 const app = express();
 
