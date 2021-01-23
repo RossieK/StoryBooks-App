0facdaeb2b8113fb19048ee08ee06ad10b02936c
RossieK <rosi.kirova96@gmail.com>
2021-01-23 22:11:35 +0200

    Add handlebars and views folder with main layout

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const exphbs = require('express-handlebars');
 const connectDB = require('./config/db');
 
 // Load config
@@ -15,6 +16,10 @@ const app = express();
 if (process.env.NODE_ENV == 'development') {
     app.use(morgan('dev'));
 }
+
+//Handlebars
+app.engine('.hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }));
+app.set('view engine', '.hbs');
 
 const PORT = process.env.PORT || 5000;
 
