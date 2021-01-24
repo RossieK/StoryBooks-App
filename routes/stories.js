29e5c6b47a865c6fab8abceea8442759fd196ce5
RossieK <rosi.kirova96@gmail.com>
2021-01-24 17:22:18 +0200

    Add add story template and route

---
+++ routes/stories.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const { ensureAuth } = require('../middleware/auth');
+const Story = require('../models/Story');
+
+//@desc Show add page
+//@route GET /stories/add
+router.get('/add', ensureAuth, (req, res) => {
+    res.render('stories/add');
+});
+
+module.exports = router;
