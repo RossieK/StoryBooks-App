6495779693e4582889049061db1bb2a2277c407c
RossieK <rosi.kirova96@gmail.com>
2021-01-24 22:10:37 +0200

    Add edit functionality

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 const exphbs = require('express-handlebars');
+const methodOverride = require('method-override');
 const passport = require('passport');
 const session = require('express-session');
 const MongoStore = require('connect-mongo')(session);
@@ -23,6 +24,15 @@ const app = express();
 //Body parser
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+//Method override
+app.use(methodOverride(function(req, res) {
+    if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+        let method = req.body._method;
+        delete req.body._method;
+        return method;
+    }
+}));
 
 //Logging
 if (process.env.NODE_ENV == 'development') {
