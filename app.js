b5a57150744b6b821b42672c355b3ce5337f039b
RossieK <rosi.kirova96@gmail.com>
2021-01-24 20:37:12 +0200

    Add edit icon

--- app.js
+++ app.js
@@ -30,14 +30,15 @@ if (process.env.NODE_ENV == 'development') {
 }
 
 //Handlebars Helpers
-const { formatDate, stripTags, truncate } = require('./helpers/hbs');
+const { formatDate, stripTags, truncate, editIcon } = require('./helpers/hbs');
 
 //Handlebars
 app.engine('.hbs', exphbs({
     helpers: {
         formatDate,
         stripTags,
-        truncate
+        truncate,
+        editIcon
     },
     defaultLayout: 'main',
     extname: '.hbs'
@@ -55,6 +56,12 @@ app.use(session({
 // Passport middleware
 app.use(passport.initialize());
 app.use(passport.session());
+
+//Set global variable
+app.use(function(req, res, next) {
+    res.locals.user = req.user || null;
+    next();
+});
 
 //Static folder
 app.use(express.static(path.join(__dirname, 'public')));
