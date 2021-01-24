dd75baf4180848e81e70780d91c0305c91851c0e
RossieK <rosi.kirova96@gmail.com>
2021-01-24 19:54:01 +0200

    Add date formatting (moment)

--- app.js
+++ app.js
@@ -29,8 +29,17 @@ if (process.env.NODE_ENV == 'development') {
     app.use(morgan('dev'));
 }
 
+//Handlebars Helpers
+const { formatDate } = require('./helpers/hbs');
+
 //Handlebars
-app.engine('.hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }));
+app.engine('.hbs', exphbs({
+    helpers: {
+        formatDate,
+    },
+    defaultLayout: 'main',
+    extname: '.hbs'
+}));
 app.set('view engine', '.hbs');
 
 //Sessions
