3d8888eaed3cf044d2751fbdb721ad4bd103f22b
RossieK <rosi.kirova96@gmail.com>
2021-01-24 11:00:19 +0200

    Add user model and passport initialization

--- README.md
+++ README.md
@@ -4,5 +4,7 @@ Application based on Node.js, Express, Handlebars, MongoDB, Passport with Google
 What you need to add to the config.env is:
 - PORT -> the port your web server will be running on; 
 - MONGO_URI -> your mongoDB URI
+- GOOGLE_CLIENT_ID -> your Google OAuth client ID
+- GOOGLE_CLIENT_SECRET -> your Google OAuth client Secret
 
 Once you install all the dependancies, you can use 'npm start' in order to run the app in a production mode and 'npm run dev' in order to run it in development mode.
--- app.js
+++ app.js
@@ -3,10 +3,15 @@ const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 const exphbs = require('express-handlebars');
+const passport = require('passport');
+const session = require('express-session');
 const connectDB = require('./config/db');
 
 // Load config
 dotenv.config({ path: './config/config.env' });
+
+//Passport config
+require('./config/passport')(passport)
 
 // Connect DB
 connectDB();
@@ -21,6 +26,17 @@ if (process.env.NODE_ENV == 'development') {
 //Handlebars
 app.engine('.hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }));
 app.set('view engine', '.hbs');
+
+//Sessions
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false
+}));
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
 
 //Static folder
 app.use(express.static(path.join(__dirname, 'public')));
