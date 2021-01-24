2b0683d3b16a70b52a445831eafaff923be143fa
RossieK <rosi.kirova96@gmail.com>
2021-01-24 20:15:16 +0200

    Add truncate and stripTags helpers

--- app.js
+++ app.js
@@ -30,12 +30,14 @@ if (process.env.NODE_ENV == 'development') {
 }
 
 //Handlebars Helpers
-const { formatDate } = require('./helpers/hbs');
+const { formatDate, stripTags, truncate } = require('./helpers/hbs');
 
 //Handlebars
 app.engine('.hbs', exphbs({
     helpers: {
         formatDate,
+        stripTags,
+        truncate
     },
     defaultLayout: 'main',
     extname: '.hbs'
